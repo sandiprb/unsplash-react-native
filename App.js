7291ef9416a0d72bc111b96699561335af8945e0
sandiprb <sandip@coverfoxmail.com>
2017-12-24 23:20:33 +0530

    Component Resturecture Continues

--- App.js
+++ App.js
@@ -1,11 +1,6 @@
 import React from 'react'
-import { StyleSheet, Text, View, ScrollView, Image, TouchableHighlight } from 'react-native'
 import { StackNavigator } from 'react-navigation'
 
-import { SearchBar, Button, WhiteSpace, WingBlank, ActivityIndicator } from 'antd-mobile'
-import AutoHeightImage from 'react-native-auto-height-image'
-
-import { getPhotoByKeyword } from './utils/Unsplash'
 import Home from './components/Home'
 import SplashScreen from './components/SplashScreen'
 
